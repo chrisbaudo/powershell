13a290df9c553df7b3f271a52d2620942f3b1b78
chrisbaudo <chrisbaudo@hotmail.com>
2023-02-03 14:23:54 -0600

    Update MoveBlobFilesPSParallel.ps1

--- powershell-storage/MoveBlobFilesPSParallel.ps1
+++ powershell-storage/MoveBlobFilesPSParallel.ps1
@@ -11,25 +11,13 @@ $blobs | ForEach-Object -Parallel {
     $baseDirectory = "sectionrate/json/"
     $lastModified = Get-Date $_.Properties.'Last-Modified'
     $year = $lastModified.Year.ToString().PadLeft(2,'0')
-	$month = $lastModified.Month.ToString().PadLeft(2,'0')
-	$day = $lastModified.Day.ToString().PadLeft(2,'0')
-	$hour = $lastModified.Hour.ToString().PadLeft(2,'0')
+    $month = $lastModified.Month.ToString().PadLeft(2,'0')
+    $day = $lastModified.Day.ToString().PadLeft(2,'0')
+    $hour = $lastModified.Hour.ToString().PadLeft(2,'0')
     $lastModifiedDirectory = "year="+$year+"/month="+$month+"/day="+$day+"/hour="+$hour+"/"
-	$fullPath = $baseDirectory+$lastModifiedDirectory
-	$oldPath = $_.name
-	$newPath = $oldPath.Replace($baseDirectory,$fullPath)
-
-    $copyuri = "https://<storageaccountname>.blob.core.windows.net/sample/" + $newpath + "<saswithquestionmark>"
-    $deleteuri = "https://<storageaccountname>.blob.core.windows.net/sample/" + $oldPath + "<saswithquestionmark>"
-    $copyheaders = @{
-        'x-ms-date' = $lastModified
-        'x-ms-version' = '2019-12-12'
-        'x-ms-copy-source' = $deleteuri
-        'Content-Length' = '0'
-    }
-    $deleteheaders = @{
-        'x-ms-version' = '2019-12-12'
-    }
+    $fullPath = $baseDirectory+$lastModifiedDirectory
+    $oldPath = $_.name
+    $newPath = $oldPath.Replace($baseDirectory,$fullPath)
 
     $folder = Get-AzDataLakeGen2Item -Context $ctx -FileSystem sample -Path $fullPath -ErrorAction SilentlyContinue
 
